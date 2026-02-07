52b19126b57ccc65f358beb3e8646e3611a7460f
adityasaini0070 <adityasaini0070@gmail.com>
2026-02-07 11:49:51 +0530

    feat: Add user registration endpoint with `AuthController` and `RegisterRequest` DTO, and remove various build and log files.

--- src/main/java/com/project/fitness/dto/RegisterRequest.java
+++ src/main/java/com/project/fitness/dto/RegisterRequest.java
@@ -1,13 +1,20 @@
 package com.project.fitness.dto;
 
 import com.project.fitness.model.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
+    @NotBlank(message = "email is required")
+    @Email(message = "invalid email")
     private String email;
+    @NotBlank(message = "password is required")
+    @Length(min = 6)
     private String password;
     private String firstName;
     private String lastName;
